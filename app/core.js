99bb5804889b5e1e7386abce7f3a3bc339c31ef3
mzmuda101-prog <m.zmuda101@gmail.com>
2026-05-22 19:51:22 +0200

    feat: update application version to 20260522-01 across all relevant files

--- app/core.js
+++ app/core.js
@@ -193,7 +193,7 @@ let aggregationWorkbenchState = {
   measureFilterValue: "",
   resultSearch: "",
 };
-const APP_BUILD_VERSION = "20260521-01";
+const APP_BUILD_VERSION = "20260522-01";
 
 const THEME_KEY = "excel-workbench-theme";
 const MAX_ROWS_KEY = "excel-workbench-max-rows";
