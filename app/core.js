d745c53da02052bc30247b7a0c8b802fdc3d3bbe
mzmuda101-prog <m.zmuda101@gmail.com>
2026-05-25 01:00:18 +0200

    feat: :lipstick: change styles of the agregation-items

--- app/core.js
+++ app/core.js
@@ -193,7 +193,7 @@ let aggregationWorkbenchState = {
   measureFilterValue: "",
   resultSearch: "",
 };
-const APP_BUILD_VERSION = "20260524-01";
+const APP_BUILD_VERSION = "20260525-01";
 
 const THEME_KEY = "excel-workbench-theme";
 const MAX_ROWS_KEY = "excel-workbench-max-rows";
