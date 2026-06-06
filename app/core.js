ccd6f6ce3feee083977f61a062e88d3e2000ec57
mzmuda101-prog <m.zmuda101@gmail.com>
2026-06-06 21:15:16 +0200

    chore(release): zaktualizowano wersję aplikacji do 20260606-04 oraz odświeżono linki do zasobów

--- app/core.js
+++ app/core.js
@@ -271,7 +271,7 @@ let aggregationWorkbenchState = {
   measureFilterValue: "",
   resultSearch: "",
 };
-const APP_BUILD_VERSION = "20260606-03";
+const APP_BUILD_VERSION = "20260606-04";
 
 const THEME_KEY = "excel-workbench-theme";
 const MAX_ROWS_KEY = "excel-workbench-max-rows";
